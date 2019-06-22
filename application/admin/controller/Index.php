0e64d8d926e0bf9318d71dbe3f762e05b7d8d89a
zgy <you@example.com>
2019-06-22 11:42:08 +0800

    zhuguangya4

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,10 +1,15 @@
 <?php
 namespace app\admin\controller;
+use gmars\rbac\Rbac;
 class Index extends Common
 {
     public function index()
     {
         return $this->fetch();
+    }
+    public function rbac(){
+    	$rbac = new Rbac();
+    	$rbac->createTable();
     }
 
    
